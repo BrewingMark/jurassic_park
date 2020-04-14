c4a0d089a02e86f754f5fe522ed303588d589aff
Mark Nicol <marknicol1991@live.co.uk>
2020-04-14 16:14:52 +0100

    Adds park constructor and tests name and ticket price.

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -5,12 +5,18 @@ const Dinosaur = require('../models/dinosaur.js');
 describe('Park', function() {
 
   beforeEach(function () {
-
+    park = new Park('Jurassic Park', 30)
   })
 
-  it('should have a name');
+  it('should have a name', function(){
+    const actual = park.name;
+    assert.strictEqual(actual, 'Jurassic Park')
+  });
 
-  it('should have a ticket price');
+  it('should have a ticket price', function(){
+    const actual = park.ticketPrice;
+    assert.strictEqual(actual, 30)
+  });
 
   it('should have a collection of dinosaurs');
 
